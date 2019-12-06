4b8b995fa0cca257baa164e3c0e5e0265f85d2e5
Miсhail h2kb Bereza <berezama@gmail.com>
2019-12-07 02:37:19 +0800

    Added common logic.

---
+++ src/RomanCalculator.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RomanCalculator {
+
+    // Translation of the expression to Reverse Polish Notation
+    static String toRPN(String expression) {
+        StringBuilder builder = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            int priority = getPriority(expression.charAt(i));
+
+            //We are working with a+b and a + b
+            if (expression.charAt(i) == ' ') {
+                continue;
+            }
+
+            //priority 0 are any numbers
+            if (priority == 0) {
+                builder.append(expression.charAt(i));
+            }
+
+            //priority 1 is a opened bracket
+            if (priority == 1) {
+                stack.push(expression.charAt(i));
+            }
+
+            //priority 2 or 3 are operators
+            if (priority > 1) {
+                builder.append(" ");
+
+                while (!stack.isEmpty()) {
+                    if (getPriority(stack.peek()) >= priority) {
+                        builder.append(stack.pop());
+                        builder.append(" ");
+                    } else {
+                        break;
+                    }
+                }
+                stack.push(expression.charAt(i));
+            }
+
+            //priority -1 is a closed bracket
+            if (priority == -1) {
+                builder.append(" ");
+
+                while (getPriority(stack.peek()) != 1) {
+                    builder.append(stack.pop());
+                }
+                stack.pop();
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            builder.append(" ");
+            builder.append(stack.pop());
+        }
+
+        return builder.toString();
+    }
+
+    // Translation of RPN to the answer
+    static int calculateResult(String expressionRNP) {
+        String[] tempStrings = expressionRNP.split(" ");
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < tempStrings.length; i++) {
+            if (isNumber(tempStrings[i])) {
+                stack.push(Integer.parseInt(tempStrings[i]));
+            } else {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+
+                switch (tempStrings[i]) {
+                    case "+":
+                        stack.push(num2 + num1);
+                        break;
+                    case "-":
+                        stack.push(num2 - num1);
+                        break;
+                    case "*":
+                        stack.push(num2 * num1);
+                        break;
+                    case "/":
+                        stack.push(num2 / num1);
+                        break;
+                }
+            }
+        }
+
+        return stack.pop();
+    }
+
+    //Get a priority of symbols or numbers
+    static int getPriority(char character) {
+        switch (character) {
+            case '*':
+            case '/':
+                return 3;
+            case '+':
+            case '-':
+                return 2;
+            case '(':
+                return 1;
+            case ')':
+                return -1;
+            default:
+                return 0;
+        }
+    }
+
+    static boolean isNumber(String numAsString) {
+        try {
+            Integer.parseInt(numAsString);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please enter a roman expression to calculate: ");
+        Scanner in = new Scanner(System.in);
+        String rawData = in.nextLine();
+        in.close();
+
+        StringBuilder builder = new StringBuilder();
+        String[] arrRawData = rawData.split(" ");
+
+        for (String str : arrRawData) {
+            if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("(") || str.equals(")")) {
+                builder.append(str);
+            } else {
+                builder.append(new RomanNumeral(str).toInt());
+            }
+        }
+
+        RomanNumeral result = new RomanNumeral(calculateResult(toRPN(builder.toString())));
+        System.out.println(result.toString());
+    }
+}
